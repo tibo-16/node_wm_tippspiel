4d0e15b829eded9b4eab408f0af257885c64e568
BuildTools <unconfigured@null.spigotmc.org>
2018-06-07 16:31:49 +0200

    Modified (final) login page

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,18 @@ router.get('/ma/', (req, res) => {
     res.render('index');
 });
 
+router.post('/ma/fabi', (req, res) => {
+    res.send('Hallo Fabi!');
+});
+
+router.post('/ma/maddin', (req, res) => {
+    res.send('Hallo Maddin!');
+});
+
+router.post('/ma/rudi', (req, res) => {
+    res.send('Hallo Rudi!');
+});
+
 router.post('/ma/tobi', (req, res) => {
     res.send('Hallo Tobi!');
 });
